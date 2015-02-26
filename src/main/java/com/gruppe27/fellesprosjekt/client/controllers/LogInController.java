cde1e0b429fd7ab08929cfedf8e0f24125e82537
laerke-s <laerkes@stud.ntnu.no>
2015-02-26 16:30:17 +0100

    made connection between fxml and controller for login. Tried to make a close function but it does not work, yet

--- src/main/java/com/gruppe27/fellesprosjekt/client/controllers/LogInController.java
+++ src/main/java/com/gruppe27/fellesprosjekt/client/controllers/LogInController.java
@@ -1,20 +1,41 @@
 package com.gruppe27.fellesprosjekt.client.controllers;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 public class LogInController extends Application{
+	
+	private Stage stage;
+	
+	@FXML
+	private TextField username;
+	@FXML
+	private PasswordField password;
+	@FXML
+	private Button ok, cancel;
+	
+	@FXML
+	private void ok(){
+		
+	}
+	
+	@FXML
+	private void cancel(){
+		stage.close();
+	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		this.stage = primaryStage;
 		Parent root = FXMLLoader.load(getClass().getResource("/fxml/LogIn.fxml"));
-        
-        Scene scene = new Scene(root, 500, 500);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+		Scene scene = new Scene(root, 300, 200);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 	
 	public static void main(String[] args) {
