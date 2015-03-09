260409330926ad876e4b48f14951f189e5fafe0b
Andreas Dørum <andreas.doerum@gmail.com>
2015-03-09 11:41:18 +0100

    add status-column to UserEvent-table

--- src/main/scripts/createdb.sql
+++ src/main/scripts/createdb.sql
@@ -53,6 +53,7 @@ CREATE TABLE TeamEvent (
 CREATE TABLE UserEvent (
   username VARCHAR(32) NOT NULL,
   event_id INT         NOT NULL,
+  status ENUM('deltar','kanskje','deltar ikke'),
   PRIMARY KEY (username, event_id),
   FOREIGN KEY (username) REFERENCES User (username),
   FOREIGN KEY (event_id) REFERENCES Event (id)
